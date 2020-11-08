4d8fad670209fa830a6bc551537bb18941b7d487
arafat08007 <aryan.it08@gmail.com>
2020-11-08 18:37:15 +0600

    add main activity

--- app/src/main/java/com/rapples/arafat/toolbox2/MainActivity.java
+++ app/src/main/java/com/rapples/arafat/toolbox2/MainActivity.java
@@ -1,55 +1,129 @@
 package com.rapples.arafat.toolbox2;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "IntentApiSample";
+    private static final String ACTION_BARCODE_DATA = "com.honeywell.sample.action.BARCODE_DATA";
+    /**
+     * Honeywell DataCollection Intent API
+     * Claim scanner
+     * Permissions:
+     * "com.honeywell.decode.permission.DECODE" */
+    private static final String ACTION_CLAIM_SCANNER = "com.honeywell.aidc.action.ACTION_CLAIM_SCANNER";
+    /**
+     * Honeywell DataCollection Intent API
+     * Release scanner claim
+     * Permissions:
+     * "com.honeywell.decode.permission.DECODE" */
+    private static final String ACTION_RELEASE_SCANNER = "com.honeywell.aidc.action.ACTION_RELEASE_SCANNER";
+    /**
+     * Honeywell DataCollection Intent API
+     * Optional. Sets the scanner to claim. If scanner is not available or if extra is not used,
+     * DataCollection will choose an available scanner. * Values : String
+     * "dcs.scanner.imager" : Uses the internal scanner
+     * "dcs.scanner.ring" : Uses the external ring scanner */
+    private static final String EXTRA_SCANNER = "com.honeywell.aidc.extra.EXTRA_SCANNER";
+    /**
+     * Honeywell DataCollection Intent API
+     * Optional. Sets the profile to use. If profile is not available or if extra is not used, * the scanner will use factory default properties (not "DEFAULT" profile properties).
+     * Values : String
+     */
+    private static final String EXTRA_PROFILE = "com.honeywell.aidc.extra.EXTRA_PROFILE";
+    /**
+     * Honeywell DataCollection Intent API
+     * Optional. Overrides the profile properties (non-persistent) until the next scanner claim. * Values : Bundle
+     */
+    private static final String EXTRA_PROPERTIES = "com.honeywell.aidc.extra.EXTRA_PROPERTIES";
+    private TextView textView;
+    private BroadcastReceiver barcodeDataReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (ACTION_BARCODE_DATA.equals(intent.getAction())) {
+                int version = intent.getIntExtra("version", 0);
+                if (version >= 1) {
+                    String aimId = intent.getStringExtra("aimId");
+                    String charset = intent.getStringExtra("charset");
+                    String codeId = intent.getStringExtra("codeId");
+                    String data = intent.getStringExtra("data");
+                    byte[] dataBytes = intent.getByteArrayExtra("dataBytes");
+                    String dataBytesStr = bytesToHexString(dataBytes);
+                    String timestamp = intent.getStringExtra("timestamp");
+                    String text = String.format("Data:%s\n" +
+                                    "Charset:%s\n" +
+                                    "Bytes:%s\n" +
+                                    "AimId:%s\n" +
+                                    "CodeId:%s\n" +
+                                    "Timestamp:%s\n",
+                            data, charset, dataBytesStr, aimId, codeId, timestamp);
+                    setText(text);
+                }
+            }
+        }
+    };
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+                textView = (TextView) findViewById(R.id.textView);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(barcodeDataReceiver, new IntentFilter(ACTION_BARCODE_DATA));
+        claimScanner();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause(); unregisterReceiver(barcodeDataReceiver);
+        releaseScanner();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    private void claimScanner() {
+        Bundle properties = new Bundle();
+        properties.putBoolean("DPR_DATA_INTENT", true); properties.putString("DPR_DATA_INTENT_ACTION", ACTION_BARCODE_DATA);
+        sendBroadcast(new Intent(ACTION_CLAIM_SCANNER) .setPackage("com.intermec.datacollectionservice")
+                .putExtra(EXTRA_SCANNER, "dcs.scanner.imager") .putExtra(EXTRA_PROFILE, "MyProfile1") .putExtra(EXTRA_PROPERTIES, properties)
+        );
+    }
+    private void releaseScanner() {
+        sendBroadcast(new Intent(ACTION_RELEASE_SCANNER)
+        );
+    }
+    private void setText(final String text) {
+        if (textView != null) {
+            runOnUiThread(new Runnable() {
+                              @Override
+                              public void run() {
+                                  textView.setText(text);
+                              }
+                          }
+            );
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    private String bytesToHexString(byte[] arr){
+        String s = "[]";
+        if(arr != null){
+            s ="[";
+            for(int i=0;i<arr.length;i++){
+                s +="0x" + Integer.toHexString(arr[i]) +", ";
+            }
+            s = s.substring(0,s.length() -2) +"]";
+        }
+        return  s;
+    }
+
 }
