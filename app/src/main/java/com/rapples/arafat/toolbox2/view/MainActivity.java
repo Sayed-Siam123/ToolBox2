2a3993e7637ea1300f8687f24062abea3e1865e8
arafat08007 <aryan.it08@gmail.com>
2020-11-11 09:54:11 +0600

    add splash screen

--- app/src/main/java/com/rapples/arafat/toolbox2/view/MainActivity.java
+++ app/src/main/java/com/rapples/arafat/toolbox2/view/MainActivity.java
@@ -1,16 +1,24 @@
-package com.rapples.arafat.toolbox2;
+package com.rapples.arafat.toolbox2.view;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.rapples.arafat.toolbox2.R;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "IntentApiSample";
+    private TextView deviceNameTv;
     private static final String ACTION_BARCODE_DATA = "com.honeywell.sample.action.BARCODE_DATA";
     /**
      * Honeywell DataCollection Intent API
@@ -76,17 +84,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
                 textView = (TextView) findViewById(R.id.textView);
+                deviceNameTv = findViewById(R.id.deviceNameTv);
+
+                getPairedDevice();
+                setupBluetoothReceiver();
+        registerReceiver(bState,new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         registerReceiver(barcodeDataReceiver, new IntentFilter(ACTION_BARCODE_DATA));
+        registerReceiver(bState,new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
         claimScanner();
     }
     @Override
     protected void onPause() {
-        super.onPause(); unregisterReceiver(barcodeDataReceiver);
+        super.onPause();
+        unregisterReceiver(barcodeDataReceiver);
+        unregisterReceiver(bState);
+
         releaseScanner();
     }
 
@@ -124,6 +142,81 @@ public class MainActivity extends AppCompatActivity {
             s = s.substring(0,s.length() -2) +"]";
         }
         return  s;
+    }
+
+    private void setupBluetoothReceiver()
+    {
+        BroadcastReceiver btReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                handleBtEvent(context, intent);
+            }
+        };
+        IntentFilter eventFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        // this is not strictly necessary, but you may wish
+        //  to know when the discovery cycle is done as well
+        eventFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(btReceiver, eventFilter);
+    }
+
+
+    private void handleBtEvent(Context context, Intent intent)
+    {
+        String action = intent.getAction();
+        Log.d("Logtag", "action received: " + action);
+
+        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            Log.i("Logtag", "found device: " + device.getName());
+        } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+            Log.d("Logtag", "discovery complete");
+        }
+
+
+    }
+
+    BroadcastReceiver bState = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+            Log.i("Logtag", "device: " + device );
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                //Device found
+                Log.i("Logtag", "Device found" + device );
+            }
+            else if (BluetoothDevice.ACTION_ACL_CONNECTED.equals(action)) {
+                //Device is now connected
+                Log.i("Logtag", "Device is now connected" + device );
+            }
+            else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                //Done searching
+                Log.i("Logtag", "Done searching" + device );
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECT_REQUESTED.equals(action)) {
+                //Device is about to disconnect
+                Log.i("Logtag", "Device is about to disconnect " + device );
+            }
+            else if (BluetoothDevice.ACTION_ACL_DISCONNECTED.equals(action)) {
+                //Device has disconnected
+                Log.i("Logtag", "Device has disconnected" + device );
+            }
+        }
+    };
+
+    void  getPairedDevice(){
+        Set<BluetoothDevice> pairedDevices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
+        int name =  BluetoothAdapter.getDefaultAdapter().getProfileConnectionState(1);
+        Log.i("Logtag", "NAME: " + name);
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice d: pairedDevices) {
+                String deviceName = d.getName();
+                String macAddress = d.getAddress();
+                Log.i("Logtag", "paired device: " + deviceName + " at " + macAddress);
+                deviceNameTv.setText(deviceName+"("+macAddress+")");
+                // do what you need/want this these list items
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/rapples/arafat/toolbox2/view/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.rapples.arafat.toolbox2.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rapples.arafat.toolbox2.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
