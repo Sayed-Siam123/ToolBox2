182f554b8c757d073c30659d68c385190741fe93
arafat08007 <aryan.it08@gmail.com>
2020-11-15 16:28:54 +0600

    add applicationSetting

---
+++ app/src/main/java/com/rapples/arafat/toolbox2/util/SharedPref.java
@@ -0,0 +1,8 @@
+package com.rapples.arafat.toolbox2.util;
+
+public class SharedPref {
+
+    public static final String SETTING_PREFERENCE="settingPreference";
+    public static final String BARCODE_COMPARISON_FUNTION ="barcodeComparisonFuntion";
+
+}
--- app/src/main/java/com/rapples/arafat/toolbox2/view/SplashActivity.java
+++ app/src/main/java/com/rapples/arafat/toolbox2/view/SplashActivity.java
@@ -2,12 +2,22 @@ package com.rapples.arafat.toolbox2.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.rapples.arafat.toolbox2.HomeActivity;
 import com.rapples.arafat.toolbox2.R;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.*;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -16,14 +26,30 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        createDirectory();
+
         Handler handler = new Handler();
 
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, HomeActivity.class));
+                startActivity(new Intent(SplashActivity.this, ApplicationSettingsActivity.class));
+                finish();
             }
         },2000);
 
     }
+
+    private void createDirectory() {
+        File mydir = this.getDir("folder", Context.MODE_PRIVATE); //Creating an internal dir;
+        File fileWithinMyDir = new File(mydir, "myfile"); //Getting a file within the dir.
+        try {
+            FileOutputStream out = new FileOutputStream(fileWithinMyDir); //Use the stream as usual to write into the file.
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 }
