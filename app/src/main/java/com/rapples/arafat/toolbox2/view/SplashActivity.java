6fba63140be279c7e9865682957930bfecb90a07
Sayed <msiam18@gmail.com>
2020-11-15 16:27:39 +0600

    Side Navigation Bar Implemented

--- app/src/main/java/com/rapples/arafat/toolbox2/view/SplashActivity.java
+++ app/src/main/java/com/rapples/arafat/toolbox2/view/SplashActivity.java
@@ -1,16 +1,32 @@
 package com.rapples.arafat.toolbox2.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.rapples.arafat.toolbox2.R;
+import com.rapples.arafat.toolbox2.databinding.ActivitySplashBinding;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ActivitySplashBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_splash);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,HomeActivity.class));
+            }
+        },3000);
+
     }
 }
