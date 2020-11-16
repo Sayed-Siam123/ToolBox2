faa43e8954c3321e4e72562ca5919f7c10337cb5
arafat08007 <aryan.it08@gmail.com>
2020-11-16 12:46:06 +0600

    save custom function data in sharedPref

--- app/src/main/java/com/rapples/arafat/toolbox2/util/SharedPref.java
+++ app/src/main/java/com/rapples/arafat/toolbox2/util/SharedPref.java
@@ -4,8 +4,13 @@ public class SharedPref {
 
     public static final String SETTING_PREFERENCE="settingPreference";
     public static final String COMPARE_PREFERENCE="comparePreference";
+    public static final String CUSTOM_FUNCTION_PREFERENCE="customFunctionPreference";
     public static final String BARCODE_COMPARISON_FUNTION ="barcodeComparisonFuntion";
+    public static final String CUSTOM_FUNCTION ="customFunction";
     public static final String BARCODE ="barcode";
+    public static final String CUSTOM_FUNCTION_NAME ="customFunctionName";
+    public static final String CUSTOM_FUNCTION_DESCRIPTION ="customFunctionDescription";
+
 
 
 
--- app/src/main/java/com/rapples/arafat/toolbox2/view/CustomFunctionSettingsActivity.java
+++ app/src/main/java/com/rapples/arafat/toolbox2/view/CustomFunctionSettingsActivity.java
@@ -4,15 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Layout;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.rapples.arafat.toolbox2.R;
 import com.rapples.arafat.toolbox2.databinding.ActivityCustomFunctionSettingsBinding;
 import com.rapples.arafat.toolbox2.model.Field;
+import com.rapples.arafat.toolbox2.util.SharedPref;
 import com.rapples.arafat.toolbox2.view.adapter.CustomFieldAdapter;
 
 import java.util.ArrayList;
@@ -24,6 +28,8 @@ public class CustomFunctionSettingsActivity extends AppCompatActivity {
     private CustomFieldAdapter adapter;
     private List<Field> fieldList;
     private int fieldSize = 1;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +40,7 @@ public class CustomFunctionSettingsActivity extends AppCompatActivity {
 
         configRecyclearview();
 
+        changeCustomFuntionStatus();
 
         setFileFormatDropDown();
 
@@ -52,6 +59,8 @@ public class CustomFunctionSettingsActivity extends AppCompatActivity {
     }
 
     private void init() {
+        sharedPreferences = getSharedPreferences(SharedPref.CUSTOM_FUNCTION_PREFERENCE,MODE_PRIVATE);
+        editor = sharedPreferences.edit();
         fieldList = new ArrayList<>();
         binding.fieldRecyclearview.setLayoutManager(new LinearLayoutManager(this));
 
@@ -67,5 +76,39 @@ public class CustomFunctionSettingsActivity extends AppCompatActivity {
     public void addField(View view) {
         fieldSize +=1;
         configRecyclearview();
+    }
+
+    public void saveData(View view) {
+        checkField();
+        
+    }
+
+    private void checkField() {
+        if(binding.nameEt.getText().toString().isEmpty()){
+            Toast.makeText(this, "Enter function name", Toast.LENGTH_SHORT).show();
+        }else if(binding.descriptionEt.getText().toString().isEmpty()){
+            Toast.makeText(this, "Enter function description", Toast.LENGTH_SHORT).show();
+        }else{
+            savaDataIntoSharedPreference();
+        }
+    }
+
+    private void savaDataIntoSharedPreference() {
+        editor.putString(SharedPref.CUSTOM_FUNCTION_NAME,binding.nameEt.getText().toString());
+        editor.putString(SharedPref.CUSTOM_FUNCTION_DESCRIPTION,binding.descriptionEt.getText().toString());
+        editor.apply();
+        binding.nameEt.setText("");
+        binding.descriptionEt.setText("");
+    }
+
+    private void changeCustomFuntionStatus() {
+
+        binding.customFuntionSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                editor.putBoolean(SharedPref.CUSTOM_FUNCTION,isChecked);
+                editor.apply();
+            }
+        });
     }
 }
