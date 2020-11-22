42cbe2bc1a9387ac44517693e1048c0b8f1a6224
arafat08007 <aryan.it08@gmail.com>
2020-11-22 15:24:08 +0600

    add scanner for search

--- app/src/main/java/com/rapples/arafat/toolbox2/view/activity/MasterDataActivity.java
+++ app/src/main/java/com/rapples/arafat/toolbox2/view/activity/MasterDataActivity.java
@@ -9,8 +9,10 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Canvas;
 import android.os.Bundle;
 import android.text.Editable;
@@ -38,19 +40,26 @@ import it.xabaras.android.recyclerview.swipedecorator.RecyclerViewSwipeDecorator
 
 public class MasterDataActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String ACTION_BARCODE_DATA = "com.honeywell.sample.action.BARCODE_DATA";
+    private static final String ACTION_CLAIM_SCANNER = "com.honeywell.aidc.action.ACTION_CLAIM_SCANNER";
+    private static final String ACTION_RELEASE_SCANNER = "com.honeywell.aidc.action.ACTION_RELEASE_SCANNER";
+    private static final String EXTRA_SCANNER = "com.honeywell.aidc.extra.EXTRA_SCANNER";
+    private static final String EXTRA_PROFILE = "com.honeywell.aidc.extra.EXTRA_PROFILE";
+    private static final String EXTRA_PROPERTIES = "com.honeywell.aidc.extra.EXTRA_PROPERTIES";
+
     private ActivityMasterDataBinding binding;
     private List<Masterdata> masterDataList;
     private List<Masterdata> newMasterDataList = new ArrayList<>();
     private RecyclerView recyclerView;
-    private RecyclerView newrecyclerView;
     private FloatingActionButton fab;
-    private  boolean isScannerOpenTrue = true;
+    private boolean isScannerOpenTrue = true;
     private EditText barCodeET;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this,R.layout.activity_master_data);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_master_data);
         fab = findViewById(R.id.fab);
         barCodeET = binding.barCodeET;
         fab.setOnClickListener(this);
@@ -61,10 +70,54 @@ public class MasterDataActivity extends AppCompatActivity implements View.OnClic
 
     }
 
+    private BroadcastReceiver barcodeDataReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (ACTION_BARCODE_DATA.equals(intent.getAction())) {
+                int version = intent.getIntExtra("version", 0);
+                if (version >= 1) {
+                    String aimId = intent.getStringExtra("aimId");
+                    String charset = intent.getStringExtra("charset");
+                    String codeId = intent.getStringExtra("codeId");
+                    String data = intent.getStringExtra("data");
+                    byte[] dataBytes = intent.getByteArrayExtra("dataBytes");
+                    String timestamp = intent.getStringExtra("timestamp");
+
+                    if (isScannerOpenTrue) {
+                        binding.barCodeFromSCET.setText(data);
+                    }
+                }
+            }
+        }
+    };
+
     @Override
     protected void onResume() {
         super.onResume();
+        registerReceiver(barcodeDataReceiver, new IntentFilter(ACTION_BARCODE_DATA));
         setDataIntoMaster();
+        claimScanner();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(barcodeDataReceiver);
+        releaseScanner();
+    }
+
+    private void claimScanner() {
+        Bundle properties = new Bundle();
+        properties.putBoolean("DPR_DATA_INTENT", true);
+        properties.putString("DPR_DATA_INTENT_ACTION", ACTION_BARCODE_DATA);
+        sendBroadcast(new Intent(ACTION_CLAIM_SCANNER).setPackage("com.intermec.datacollectionservice")
+                .putExtra(EXTRA_SCANNER, "dcs.scanner.imager").putExtra(EXTRA_PROFILE, "MyProfile1").putExtra(EXTRA_PROPERTIES, properties)
+        );
+    }
+
+    private void releaseScanner() {
+        sendBroadcast(new Intent(ACTION_RELEASE_SCANNER)
+        );
     }
 
     private void setDataIntoMaster() {
@@ -72,12 +125,12 @@ public class MasterDataActivity extends AppCompatActivity implements View.OnClic
         MasterExecutor.getInstance().diskIO().execute(new Runnable() {
             @Override
             public void run() {
-                masterDataList  =  MasterData_DB.getInstance(getApplicationContext()).MasterdataDao().loadAllData();
+                masterDataList = MasterData_DB.getInstance(getApplicationContext()).MasterdataDao().loadAllData();
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         recyclerView = (RecyclerView) findViewById(R.id.masterDataRecyclerview);
-                        CustomMasterDataAdapter adapter = new CustomMasterDataAdapter(masterDataList,MasterDataActivity.this);
+                        CustomMasterDataAdapter adapter = new CustomMasterDataAdapter(masterDataList, MasterDataActivity.this);
                         recyclerView.setHasFixedSize(true);
                         recyclerView.setLayoutManager(new LinearLayoutManager(MasterDataActivity.this));
                         recyclerView.setAdapter(adapter);
@@ -102,7 +155,7 @@ public class MasterDataActivity extends AppCompatActivity implements View.OnClic
                     @Override
                     public void run() {
                         recyclerView = (RecyclerView) findViewById(R.id.masterDataRecyclerview);
-                        CustomMasterDataAdapter adapter = new CustomMasterDataAdapter(newMasterDataList,MasterDataActivity.this);
+                        CustomMasterDataAdapter adapter = new CustomMasterDataAdapter(newMasterDataList, MasterDataActivity.this);
                         recyclerView.setHasFixedSize(true);
                         recyclerView.setLayoutManager(new LinearLayoutManager(MasterDataActivity.this));
                         recyclerView.setAdapter(adapter);
@@ -130,20 +183,20 @@ public class MasterDataActivity extends AppCompatActivity implements View.OnClic
 
             switch (direction) {
                 case ItemTouchHelper.RIGHT:
-                    startActivity(new Intent(MasterDataActivity.this,EditMasterdataActivity.class)
-                            .putExtra("barcode",masterDataList.get(position).getBarcode())
-                            .putExtra("description",masterDataList.get(position).getDescription())
-                            .putExtra("price",masterDataList.get(position).getPrice())
-                            .putExtra("image",masterDataList.get(position).getImage())
-                            .putExtra("id",String.valueOf(masterDataList.get(position).getId()))
+                    startActivity(new Intent(MasterDataActivity.this, EditMasterdataActivity.class)
+                            .putExtra("barcode", masterDataList.get(position).getBarcode())
+                            .putExtra("description", masterDataList.get(position).getDescription())
+                            .putExtra("price", masterDataList.get(position).getPrice())
+                            .putExtra("image", masterDataList.get(position).getImage())
+                            .putExtra("id", String.valueOf(masterDataList.get(position).getId()))
 
                     );
 
 
                     break;
 
-                    case ItemTouchHelper.LEFT:
-                    Snackbar.make(recyclerView,  "Delete product", Snackbar.LENGTH_LONG)
+                case ItemTouchHelper.LEFT:
+                    Snackbar.make(recyclerView, "Delete product", Snackbar.LENGTH_LONG)
                             .setAction("Undo", new View.OnClickListener() {
                                 @Override
                                 public void onClick(View view) {
@@ -181,7 +234,7 @@ public class MasterDataActivity extends AppCompatActivity implements View.OnClic
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                       setDataIntoMaster();
+                        setDataIntoMaster();
                     }
                 });
             }
@@ -196,7 +249,7 @@ public class MasterDataActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.fab){
+        if (v.getId() == R.id.fab) {
             Log.d("TAG", "onClick: ADD");
             startActivity(new Intent(MasterDataActivity.this, AddMasterDataActivity.class));
         }
@@ -245,47 +298,82 @@ public class MasterDataActivity extends AppCompatActivity implements View.OnClic
         imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 
-    public void search(){
-        barCodeET.addTextChangedListener(new TextWatcher() {
+    public void search() {
 
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+        if (isScannerOpenTrue) {
+            binding.barCodeFromSCET.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                }
 
-            }
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    newMasterDataList.clear();
+                    for (Masterdata productListData : masterDataList) {
+                        if (productListData.getBarcode().toLowerCase().contains(s) || productListData.getDescription().toLowerCase().contains(s)) {
+                            newMasterDataList.add(productListData);
+                        }
+                        if (newMasterDataList.size() == 0) {
+                            setDataIntoMaster();
+                        } else {
+                            for (int i = 0; i < newMasterDataList.size(); i++) {
+                                setNewDataIntoMaster();
+                            }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                Log.d("Changed text", "afterTextChanged: "+s.toString()+" count: "+count);
-                newMasterDataList.clear();
-
-                for(Masterdata productListData : masterDataList){
-
-                    if(productListData.getBarcode().toLowerCase().contains(s) || productListData.getDescription().toLowerCase().contains(s)){
-                        newMasterDataList.add(productListData);
-                    }
-
-
-                    if(newMasterDataList.size()==0){
-                        Log.d("status", "onTextChanged: no data found");
-                        setDataIntoMaster();
-                    }else {
-
-                        Log.d("status", "onTextChanged: data found");
-                        for(int i = 0; i<newMasterDataList.size();i++){
-                            Log.d("TAG", "onTextChanged: "+newMasterDataList.get(i).getBarcode());
-                            setNewDataIntoMaster();                        }
+                        }
 
                     }
 
                 }
 
-            }
+                @Override
+                public void afterTextChanged(Editable s) {
 
-            @Override
-            public void afterTextChanged(Editable s) {
+                }
+            });
 
-            }
-        });
+        } else {
+            barCodeET.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    Log.d("Changed text", "afterTextChanged: " + s.toString() + " count: " + count);
+                    newMasterDataList.clear();
+
+                    for (Masterdata productListData : masterDataList) {
+
+                        if (productListData.getBarcode().toLowerCase().contains(s) || productListData.getDescription().toLowerCase().contains(s)) {
+                            newMasterDataList.add(productListData);
+                        }
+
+
+                        if (newMasterDataList.size() == 0) {
+                            Log.d("status", "onTextChanged: no data found");
+                            setDataIntoMaster();
+                        } else {
+
+                            Log.d("status", "onTextChanged: data found");
+                            for (int i = 0; i < newMasterDataList.size(); i++) {
+                                Log.d("TAG", "onTextChanged: " + newMasterDataList.get(i).getBarcode());
+                                setNewDataIntoMaster();
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                @Override
+                public void afterTextChanged(Editable s) {
+
+                }
+            });
+        }
     }
 
 
