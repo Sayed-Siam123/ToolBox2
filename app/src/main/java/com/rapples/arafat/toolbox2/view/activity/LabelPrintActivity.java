44464947263394ac8f7e159fe3132927c5bd12c2
Sayed <msiam18@gmail.com>
2020-12-01 15:14:02 +0600

    label printing

--- app/src/main/java/com/rapples/arafat/toolbox2/view/activity/LabelPrintActivity.java
+++ app/src/main/java/com/rapples/arafat/toolbox2/view/activity/LabelPrintActivity.java
@@ -10,8 +10,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
 
 import com.rapples.arafat.toolbox2.Database.MasterData_DB;
 import com.rapples.arafat.toolbox2.Database.MasterExecutor;
@@ -31,6 +34,7 @@ public class LabelPrintActivity extends AppCompatActivity {
     private boolean flag = false;
     private boolean ScannerOpen = true;
     private List<Masterdata> masterDataList;
+    private int quantity = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,11 +109,70 @@ public class LabelPrintActivity extends AppCompatActivity {
 
         else{
             Log.d("TAG", "checkDatainMasterList: ");
-
-
+            inputMasterTypeBarcode();
         }
     }
 
 
+    private void inputMasterTypeBarcode() {
 
+        binding.masterbarCodeFromET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    Log.d("TAG", "onEditorAction: "+ binding.masterbarCodeFromET.getText().toString());
+                    for (int i = 0; i < masterDataList.size(); i++) {
+                        Log.d("TAG", "onTextChanged: " + masterDataList.get(i).getBarcode());
+
+                        if(masterDataList.get(i).getBarcode().equals(binding.masterbarCodeFromET.getText().toString())){
+                            Log.d("TAG", "onEditorAction: exist");
+                            binding.afterInputLL.setVisibility(View.VISIBLE);
+                            binding.editbarcodeLL.setVisibility(View.GONE);
+                            binding.scanMastercodeTV.setVisibility(View.GONE);
+                            binding.underline3.setVisibility(View.GONE);
+                            binding.underline.setVisibility(View.VISIBLE);
+                            binding.fabButton1.setVisibility(View.VISIBLE);
+                            binding.quantityPrint.setVisibility(View.VISIBLE);
+                            binding.quantityTV.setText(String.valueOf(quantity));
+                            binding.barcodeET1.setText(masterDataList.get(i).getBarcode());
+                            binding.underline2.setVisibility(View.VISIBLE);
+                            binding.barcodeET1.requestFocus();
+                            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                            imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
+                            binding.descTV.setText(masterDataList.get(i).getDescription());
+                            binding.priceTV.setText(masterDataList.get(i).getPrice());
+
+                        }
+
+                        else{
+                            Log.d("TAG", "onEditorAction: not exist");
+                        }
+                    }
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+    }
+
+    public void fabButtonClicktoPrint(View view) {
+        Log.d("TAG", "fabButtonClicktoPrint: Print! Print!! Print!!! "+quantity);
+
+    }
+
+    public void decrementQuantity(View view) {
+        if(quantity != 1){
+            quantity--;
+            binding.quantityTV.setText(String.valueOf(quantity));
+        }
+    }
+
+    public void incrementQuantity(View view) {
+        if(quantity >= 1 && quantity != 10){
+            quantity++;
+            binding.quantityTV.setText(String.valueOf(quantity));
+        }
+    }
 }
